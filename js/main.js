4e08cb28c9f87f57f9970a2b1516e611e1f66b57
kevin <kevin.rojokevin17@gmail.com>
2024-05-06 17:50:02 -0300

    Avanzando

--- js/main.js
+++ js/main.js
@@ -1,40 +1,69 @@
-const form = document.querySelector(".formulario-1");
-const form2 = document.querySelector(".formulario-2");
-const continue1 = document.querySelector(".continuar1");
-const punto2 = document.querySelector(".pnt2");
+/*-------JavaScrip para primer formulario ----*/
 
-continue1.addEventListener("click", () => {
+const form = document.querySelector(".formulario-1");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+const siguiente = document.querySelector(".continuar1");
+const form2 = document.querySelector(".formulario-2");
+
+siguiente.addEventListener("click", () => {
   form2.classList.remove("formulario");
   form.classList.add("formulario");
-  punto2.classList.add("punto-1");
 });
 
-const continue2 = document.querySelector(".continuar2");
-const punto3 = document.querySelector(".pnt3");
-const form3 = document.querySelector(".formulario-3");
+/*-------JavaScrip para segundo formulario ----*/
 
-continue2.addEventListener("click", () => {
-  punto3.classList.add("punto-1");
-  form3.classList.remove("formulario");
-  form2.classList.add("formulario");
+let opt1 = document.querySelector(".selec1");
+let opt2 = document.querySelector(".selec2");
+let opt3 = document.querySelector(".selec3");
+let opciones = [];
+const limite = 2;
 
-  mostrarDatos();
-});
-
-function mostrarDatos() {
-  const nombre = document.querySelector(".name").value;
-  const email = document.querySelector(".email").value;
-  const selec1 = document.querySelector(".selec1").value;
-  const selec2 = document.querySelector(".selec2").value;
-  const selec3 = document.querySelector(".selec3").value;
-  const form3 = document.querySelector(".formulario-3");
-
-  form3.innerHTML = `<h2 class="title">Datos</h2>
- <p>Nombre: <br> ${nombre} </p>
- <p>Email: <br> ${email} </p>
- <p>Insteres:</p>
- <ul>
-   <li>${selec1}</li>
- </ul>
- <input type="button" value="Confirm" class="continuar3" />`;
+function arregar(elemento) {
+  if (opciones.length < limite) {
+    console.log("agregado", elemento);
+  } else {
+    opciones.plice();
+    console.log("no se puede");
+  }
 }
+
+opt1.addEventListener("click", () => {
+  opciones.push(opt1.textContent);
+  console.log(opciones);
+  arregar();
+});
+opt2.addEventListener("click", () => {
+  opciones.push(opt2.textContent);
+  console.log(opciones);
+  arregar();
+});
+opt3.addEventListener("click", () => {
+  opciones.push(opt3.textContent);
+  arregar();
+});
+form2.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+/*-------JavaScrip para tercer formulario ----*/
+
+let siguiente2 = document.querySelector(".continuar2");
+
+siguiente2.addEventListener("click", () => {
+  const form3 = document.querySelector(".formulario-3");
+  const name = document.querySelector(".name").value;
+  const email = document.querySelector(".email").value;
+
+  opciones.forEach((opcion) => {
+    form3.innerHTML = `<h3 class:"text-2">Datos</h3><br>
+    <p>Nombre:<br>${name}</p><br>
+    <p>Email:<br>${email}</p><br>
+    <p>Especialidad</p>
+    <ul><li>${opcion}</li></ul><br>
+    <input type="submit" value="Enviar" class="continuar3" required />`;
+  });
+});
