8c330e1cb6256cd95c53213c3cd167e8fcf351eb
kevin <kevin.rojokevin17@gmail.com>
2024-05-11 16:46:56 -0300

    falta movil

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const siguiente = document.querySelector(".continuar1");
 const form2 = document.querySelector(".formulario-2");
 const punto2 = document.querySelector(".pnt2");
 const text = document.querySelector(".text-2");
+const delet = document.querySelector(".next");
 
 siguiente.addEventListener("click", () => {
   const name = document.querySelector(".name").value;
@@ -20,8 +21,8 @@ siguiente.addEventListener("click", () => {
   } else {
     form2.classList.remove("formulario");
     form.classList.add("formulario");
-    punto2.classList.add("punto-1");
-    text.innerText = "step 2 of 3";
+    delet.innerHTML =
+      "<p class='text-2'>step 2 of 3</p><div class='puntos'><img src='./img/circulo-negro.png' alt='' class='punto-1' /><img src='img/circulo-negro.png' alt='' class='punto-1' /><img src='img/circulo.png' alt='' class='pnt3' /></div>";
   }
 });
 
@@ -41,7 +42,7 @@ function arregar(elemento) {
     console.log("no se puede");
   }
 }
-
+console.log(opciones);
 opt1.addEventListener("click", () => {
   opciones.push(opt1.textContent);
   console.log(opciones);
@@ -70,20 +71,31 @@ siguiente2.addEventListener("click", () => {
   const email = document.querySelector(".email").value;
   const punto3 = document.querySelector(".pnt3");
 
-  if (form2 == "") {
-    alert("Tiene que completar datos");
+  if (opciones == 0) {
+    Swal.fire({
+      icon: "error",
+      title: "Faltan datos",
+      text: "Elije una opción",
+    });
   } else {
     form3.classList.remove("formulario");
     form2.classList.add("formulario");
     punto3.classList.add("punto-1");
-    text.innerText = "step 3 of 3";
+    delet.innerHTML =
+      "<p class='text-2'>step 3 of 3</p><div class='puntos'><img src='./img/circulo-negro.png' alt='' class='punto-1' /><img src='img/circulo-negro.png' alt='' class='punto-1' /><img src='img/circulo-negro.png' alt='' class='punto-1' /></div>";
 
     opciones.forEach((opcion) => {
-      form3.innerHTML = `<h3 class:"text-2">Datos</h3>
-    <p>Nombre:<br>${name}</p>
-    <p>Email:<br>${email}</p>
-    <p>Especialidad</p>
-    <ul><li>${opcion}</li></ul>
+      form3.innerHTML = `<h2 class="title">Summary</h2>
+    <div class="datos">
+    <p class="nombre">Nombre:
+    <p class="datosNombre">${name}</p>
+    </p>
+    <p class="correo">Email:
+    <p class="datosEmail">${email}</p>
+    </p>
+    </div>
+    <p>Especialidad:</p>
+    <ul><li class="datosEspecialidad">${opcion}</li></ul>
     <input type="submit" value="Enviar" class="continuar3" required />`;
     });
   }
